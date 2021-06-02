cfdc02413f91171e262524a32b7d40b7d5342e54
lidc1004 <lidc1004@163.com>
2021-06-02 16:22:58 +0800

    VOC数据转为yolo的txt格式

---
+++ voc_to_yolo.py
@@ -0,0 +1,47 @@
+import xml.etree.ElementTree as ET
+from os import getcwd
+from tqdm import tqdm
+sets = [('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+classes = ['human', 'boat', 'bicycle', 'paddle', 'guitar', 'hockey stick', 'cymbal', 'skis', 'soccer', 'combine with glove', 
+          'sailboat', 'kite', 'baseball bat', 'skateboard', 'surfboard', 'piano', 'tennis racket', 'skating and skiing shoes', 
+          'baseball', 'snowboard', 'american football', 'basketball', 'billiards', 'tennis ball', 'frisbee', 'golf club', 
+          'violin', 'golf ball', 'saxophone', 'trumpet', 'cue', 'volleyball', 'trombone', 'treadmill', 'tuba', 'flute', 
+          'barbell', 'table tennis  paddle', 'french horn', 'horse', 'fish', 'wild bird', 'cow', 'sheep', 'dog', 'elephant', 
+          'cat', 'zebra', 'duck', 'giraffe', 'pigeon', 'goose', 'deer', 'chicken', 'penguin', 'swan', 'bear', 'camel', 'goldfish', 
+          'antelope', 'shrimp', 'jellyfish', 'pig', 'dolphin', 'donkey', 'tortoise', 'parrot', 'shark', 'llama', 'crab', 'lion', 
+          'monkey', 'eagle', 'rabbit', 'lobster', 'bread', 'apple', 'cake', 'orange', 'banana', 'tomato', 'donut', 'pizza', 'broccoli', 
+          'lemon', 'pumpkin', 'carrot', 'cookies', 'candy', 'strawberry', 'grapes', 'pear', 'potato', 'pie', 'hamburger', 'cucumber', 
+          'hot dog', 'mango', 'egg', 'onion', 'ice cream', 'watermelon', 'cabbage', 'pine apple', 'peach', 'french fries', 'sandwich', 
+          'kiwi fruit', 'cherry', 'corn', 'eggplant', 'dates', 'rice', 'meat balls', 'pomegranate', 'coconut', 'egg tart', 'cheese', 
+          'pomelo', 'papaya', 'sushi', 'spring rolls', 'steak', 'radish', 'noodles', 'baozi', 'pitaya', 'durian', 'pudding', 
+          'macron', 'cathead', 'doghead', 'pingpang']
+
+def convert_annotation(year, image_id, list_file):
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id), encoding='utf-8')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = 0 
+        if obj.find('difficult')!=None:
+            difficult = obj.find('difficult').text
+            
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+
+for year, image_set in sets:
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    for image_id in tqdm(image_ids):
+        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        convert_annotation(year, image_id, list_file)
+        list_file.write('\n')
+    list_file.close()
